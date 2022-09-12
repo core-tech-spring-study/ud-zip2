d1b3b22711d38a2f9d12b12befed698c19d1619a
yjlee <susan7744850@gmail.com>
2022-09-12 14:10:25 +0900

    조회한 빈이 모두 필요할 때, List,Map

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,10 @@
 package hello.core.discount;
 
-import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private static final int discountPercent = 10;
